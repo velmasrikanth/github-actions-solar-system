d90dafa1c52d2547da47634715792ac62635c1c6
velma srikanth <velmasrikanth@gmail.com>
2025-11-06 14:28:19 +0530

    env added

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect('mongodb+srv://supercluster.d83jj.mongodb.net/superData', {
-    user: 'superuser',
-    pass: 'SuperPassword',
+mongoose.connect('process.env.MONGO_URI', {
+    user: 'process.env.MONGO_USER',
+    pass: 'process.env.MONGO_PASSWORD',
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, function(err) {
