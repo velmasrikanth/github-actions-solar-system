3d9ca3fb68a3fda3f556d9c2008d7bc053190001
velma srikanth <velmasrikanth@gmail.com>
2025-11-06 14:36:32 +0530

    vars changed

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
 mongoose.connect('process.env.MONGO_URI', {
-    user: 'process.env.MONGO_USER',
+    user: 'process.env.MONGO_USERNAME',
     pass: 'process.env.MONGO_PASSWORD',
     useNewUrlParser: true,
     useUnifiedTopology: true
